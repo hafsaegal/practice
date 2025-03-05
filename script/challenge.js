b0849042c9001e871db8216a2b1f03430bf69688
Hafsa Egal <hafsaegal915@gmail.com>
2025-03-05 14:43:34 +0300

    challenge

---
+++ script/challenge.js
@@ -0,0 +1,66 @@
+// const number= parseInt(prompt("Enter a number:"));
+
+// if (number % 2 === 0) {
+//     console.log(`${number} is even.`);
+// }else {
+//     console.log(`${number} is odd.`);
+// }
+
+// let age=18;
+
+// if(age>=18) {
+//     console.log("You are eligible to vote")
+// }else{
+//     console.log("You are not eligible to vote")
+// }
+
+// const number = parseInt(prompt("Enter a number:"));
+
+// if(number >=1) {
+//     console.log("Positive")
+// } else if(number <0) {
+//     console.log("Negative")
+// }else if(number=0) {
+//     console.log("Zero")
+// }
+
+
+// let score=35;
+// if(score>=50) {
+//     console.log("You passed!")
+// }else{
+//     console.log("You failed")
+// }
+
+// const num1= Number(prompt("Enter the first number:"));
+// const num2= Number(prompt("Enter the second number:"));
+
+// if (num1>num2) {
+//     console.log(`${num1} is greater than ${num2}`)
+
+// }else if (num1<num2) {
+//     console.log(`${num2} is greater than ${num1}`);
+// }else {
+//     console.log("Both numbers are equal")
+// }
+
+// const num1= Number(prompt("Enter the first number"));
+// const num2 = Number(prompt("Enter the second number"));
+// const num3 = Number(prompt("Enter the third number"));
+
+// const largest= Math.max(num1,num2,num3);
+// console.log(`The largest number is : ${largest}`);
+
+let grade=71;
+
+if(grade> 90) {
+    console.log("A")
+}else if (grade>80){
+    console.log("B")
+}else if (grade>70) {
+    console.log("c")
+}else if (grade>60) {
+    console.log("D")
+}else (grade<60) 
+    console.log("F")
+
---
+++ script/index.js
@@ -0,0 +1,25 @@
+// // function razzle(lawyer="Billy", target="'em") {
+// //   console.log(`${lawyer } razzle-dazzle ${target}!`);
+// // }
+// // razzle();
+// // razzle("Methuselah", "T'challah");
+
+// // function saturdayFun() {
+// //   return "This saturday, I want to "
+
+// // }
+// // saturdayFun();
+
+// // function mondayWork() {
+// //   return "This Monday, i will go to the office"
+// // };
+// // mondayWork();
+
+// function outer(greeting, msg= "It's a fine day to learn"){
+//   const innerFunction = function(name, lang='Python'){
+//     return `${greeting}, ${name}! ${msg} ${lang}`;
+//   };
+//   return innerFunction("student","Javascript");
+// }
+
+// outer("Hello");
